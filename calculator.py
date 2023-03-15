7648e3ef0ed979e1eef6910f4a01ee59b349dd2b
Yashal <yashalimran506@gmail.com>
2023-03-15 13:06:12 +0500

    second commit

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
-import numpy as np
 import math
-
+import numpy as np
 
 class Calculator:
+
     def add(self):
         n = int(input("enter total numbers to add: "))
         sum1 = 0
@@ -10,7 +10,6 @@ class Calculator:
             num = input(f'enter number {i}: ')
             sum1 = sum1 + i
         print("Result of Addition: ", sum1)
-
 
     def subtract(self):
         print("\n Subtraction")
@@ -75,62 +74,69 @@ class Calculator:
         result = math.atan(res)
         print(f"Tan({n})={result}")
 
+class Evalpostfix:  # postfix code
+    def __init__(self):
+        self.stack = []
+        self.top = -1
+
+    def pop(self):
+        if self.top == -1:
+            return
+        else:
+            self.top = self.top - 1
+            return self.stack.pop()
+
+    def push(self, i):
+        self.top = self.top + 1
+        self.stack.append(i)
+
+    def centralfunc(self, ab):
+        for i in ab:
+            try:
+                self.push(int(i))
+            except ValueError:
+                val1 = self.pop()
+                val2 = self.pop()
+                if i == '/':
+                    self.push(val2 / val1)
+                else:
+                    switcher = {'+': val2 + val1, '-': val2 - val1, '*': val2 * val1, '^': val2 ** val1}
+                    self.push(switcher.get(i))
+        return int(self.pop())
 
 
+obj1 = Calculator()
+while True:
+    x = ('\n1. Add \n2. Sub \n3. Multiply \n4. Divide \n5. Sin \n6. Cos \n7. Tan \n8. Sec \n9. Cosec \n10. Cot')
+    print(x)
+    choice = int(input('Please select from 0 to 10: '))
+    if choice == 1:
+        obj1.add()
+    elif choice == 2:
+        obj1.subtract()
+    elif choice == 3:
+        obj1.multiply()
+    elif choice == 4:
+        obj1.divide()
+    elif choice == 5:
+        obj1.sin()
+    elif choice == 6:
+        obj1.cosine()
+    elif choice == 7:
+        obj1.tan()
+    elif choice == 8:
+        obj1.secant()
+    elif choice == 9:
+        obj1.cosecant()
+    elif choice == 10:
+        obj1.cotangent()
+    elif choice == 0:
+        break
+    else:
+        print("Please select from 0 to 10.")
 
-a = Calculator()
-a.add()
-a.subtract()
-a.multiply()
-a.divide()
-a.sin()
-a.cosine()
-a.tan()
-a.secant()
-a.secant()
-a.cosecant()
-a.cotangent()
-
-
-# postfix code
-
-
-class evalpostfix:
-	def __init__(self):
-		self.stack =[]
-		self.top =-1
-	def pop(self):
-		if self.top ==-1:
-			return
-		else:
-			self.top = self.top - 1
-			return self.stack.pop()
-	def push(self, i):
-		self.top = self.top + 1
-		self.stack.append(i)
-
-	def centralfunc(self, ab):
-		for i in ab:
-			try:
-				self.push(int(i))
-			except ValueError:
-				val1 = self.pop()
-				val2 = self.pop()
-
-
-				if i == '/':
-				    self.push(val2 / val1)
-				else:	
-				    switcher = {'+':val2 + val1, '-':val2-val1, '*':val2 * val1, '^':val2**val1}
-				    self.push(switcher.get(i))
-		return int(self.pop())
-
-str ='200 100 - 12 / 35 - 97 +'
-
-strconv = str.split(' ')
-obj = evalpostfix()
+string = '200 100 - 12 / 35 - 97 +'
+strconv = string.split(' ')
+obj = Evalpostfix()
+print("\nResult of given infix expression to postfix:")
 print(obj.centralfunc(strconv))
-
-
-
-
