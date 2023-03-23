1a01f6472050658078a11def14f368e1c1724317
Yashal <yashalimran506@gmail.com>
2023-03-23 22:43:47 +0500

    Second Commit

--- Weatherman.py
+++ Weatherman.py
@@ -1,98 +1,140 @@
+import csv
 import glob
-import matplotlib.pyplot as plt
 import os
-import pandas as pd
-class Weatherman:
-    def parser():
-        global final_df, df
-        final_df = pd.DataFrame()
-        folder_path = '/Users/yashal.imran/Downloads/weatherfiles/'
-        # Get a list of all files in the folder with extensions .tsv, .txt, and .xlsx
-        files = glob.glob(os.path.join(folder_path, "*.tsv")) + \
-                glob.glob(os.path.join(folder_path, "*.txt")) + \
-                glob.glob(os.path.join(folder_path, "*.xlsx"))
 
-        for file in files:  # Loop through each file in the list and read it using pandas
-            if file.endswith(".tsv"):
+result = []
 
-                df = pd.read_csv(file, delimiter="\t", )
-                final_df = pd.concat([final_df, df], ignore_index=True)
+def parser(file):
 
-                # print_all_rows = pd.set_option('display.max_rows', None)
-                # print(print_all_rows)
-                # print_all_cols = pd.set_option('display.max_columns', None)
-                # print(print_all_cols)
+    list_of_data = []
+    with open(file, 'r') as file:
+        reader = csv.DictReader(file)
+        for rows in reader:
+            list_of_data.append(rows)
+            print(list_of_data)
 
-            elif file.endswith(".txt"):  # Read TXT file using pandas
-                df = pd.read_csv(file)
-                final_df = pd.concat([final_df, df], ignore_index=True)
+    return list_of_data
 
-            elif file.endswith(".xlsx"):
-                df = pd.read_excel(file)
-                final_df = pd.concat([final_df, df], ignore_index=True)
+def yearwise_temperature(year):
 
-        final_df.shape
-        print(final_df)
+    folder_path = '/Users/yashal.imran/Downloads/weatherfiles/'
+    path = '*' + str(year) + '_*txt'  # search for files in directory
+    files = glob.glob(folder_path + path)
+    length = len(files)
 
-    def yearwise_temperature():
-        folder_path = '/Users/yashal.imran/Downloads/weatherfiles/'
-        year = input('Enter the year of interest: ')
-        path = '*' + str(year) + '_*.*'
-        files = glob.glob(folder_path + '/' + path)
+    max_file = 0
+    max_temp = 0
+    min_file = 0
+    min_temp = 0
+    humidity_file = 0
+    humidity = 0
+    date_max_temp = 0
+    date_min_temp = 0
+    date_humidity = 0
 
-        final_df['PKT'] = pd.to_datetime(df['PKT'])  #convert a column in a DataFrame to a pandas datetime format.
-        final_df['PKT'] = final_df['PKT'].dt.year
+    for traverse in range(length):
+        traverse_files = files[traverse]
 
-        max_temp = final_df.groupby('PKT')['Max TemperatureC'].max().reset_index()
-        print(max_temp)
+        read_files = parser(traverse_files)
 
-        min_temp = final_df.groupby('PKT')['Min TemperatureC'].min().reset_index()
-        print(min_temp)
+        for read in read_files:
+            max_file = read['Max TemperatureC']
 
-        humidity = final_df.groupby('PKT')['Max Humidity'].max().reset_index()
-        print(humidity)
+            if max_file == '':
+                max_file = 0
+            if float(max_file) > max_temp:
+                max_temp = float(max_file)
+                date_max_temp = read['PKT']
 
-    def month_wise_temperature():
-        folder_path = '/Users/yashal.imran/Downloads/weatherfiles/'
-        month = str(input('Enter the month of interest: '))
-        path = '*' + str(month) + '_*.*'
-        files = glob.glob(folder_path + '/' + path)
+        for read in read_files:
+            min_file = read['Min TemperatureC']
 
-        for i in files:
-            final_df['PKT'] = pd.to_datetime(df['PKT'])
-            final_df['PKT'] = final_df['PKT'].dt.month
+            if min_file == '':
+                min_file = 0
+            if float(min_file) > min_temp:
+                min_temp = float(min_file)
+                date_min_temp = read['PKT']
 
-            max_highest_temp = final_df.groupby('PKT')['Mean TemperatureC'].max().reset_index()
-            avg_max_mean_temp = max_highest_temp.mean()
+        for read in read_files:
+            humidity_file = read['Max Humidity']
 
-            min_lowest_temp = final_df.groupby('PKT')['Mean TemperatureC'].min().reset_index()
-            avg_min_mean_temp = min_lowest_temp.mean()
+            if humidity_file == '':
+                humidity_file = 0
+            if float(humidity_file) > humidity:
+                humidity = float(humidity_file)
+                date_humidity = read['PKT']
 
-            avg_mean_humidity = final_df.groupby('PKT')['Mean Humidity'].mean().reset_index()
+    print(f"\n{max_temp} on {date_max_temp}")
+    print(f"\n{min_temp} on {date_min_temp}")
+    print(f"\n{humidity} on {date_humidity}")
+
+    result.append(max_temp)
+    result.append(min_temp)
+    result.append(humidity)
+
+def monthwise_temperature(file):
+
+    add_max_temp = add_min_temp = add_humidity = 0
+    read_files = parser(file)
+    length = len(parser(file))
+
+    for read in read_files:
+        max_file = read['Max TemperatureC']
+
+        min_file = read['Min TemperatureC']
+        humidity_file = read['Max Humidity']
+
+        if max_file == '' or min_file == '' or humidity_file == '':
+            max_file = 0
+            min_file = 0
+            humidity_file = 0
+        else:
+            add_max_temp += float(max_file)
+            add_min_temp += float(min_file)
+            add_humidity += float(humidity_file)
+
+    average_max = add_max_temp / length
+    average_min = add_min_temp / length
+    average_humidity = add_humidity / length
+
+    print("\nAverage max temperature", average_max)
+    print("\nAverage min temperature", average_min)
+    print("\nAverage humidity", average_humidity)
+
+    result.append(average_max)
+    result.append(average_min)
+    result.append(average_humidity)
+
+def charts(file):
+
+    read_files = parser(file)
+    row = '+ '
+    string = 0
+    string2 = 0
+    max_temp = 0
+    max_temp = 0
+    length = len(read_files)
+
+    for read_length in range(length):
+        for read in read_files:
+            date = read['PKT']
+
+            max_temp = read['Max TemperatureC']
+            min_temp = read['Min TemperatureC']
+            if max_temp == '':
+                max_temp = 0
+            if min_temp == '':
+                min_temp = 0
+
+            string = int(max_temp) * row
+            string2 = int(min_temp) * row
+
+    print(f"\n{date} {string} {max_temp}")
+    print(f"\n{date} {string2} {min_temp}")
 
 
-        print('\nmean_highest_temp\n', avg_max_mean_temp)
-        print("avg_min_mean_temp", avg_min_mean_temp)
-        print("avg_mean_humidity", avg_mean_humidity)
-
-    def charts():
-        month = int(input("Enter month: "))
-        folder_path = '/Users/yashal.imran/Downloads/weatherfiles/'
-        month = int(input("Enter month: "))
-        path = '*' + str(month) + '_*.*'
-        files = glob.glob(folder_path + '/' + path)
-
-        for i in files:
-            if final_df['PKT'] == month:
-
-                max_temp = final_df['Max TemperatureC'].plot.barh(color=red)
-                print(max_temp)
-
-                min_temp = final_df['Min TemperatureC'].plot.barh(color=red)
-                print(min_temp)
-
-obj = Weatherman
-obj.parser()
-obj.yearwise_temperature()
-obj.month_wise_temperature()
-obj.charts()
+file_path = '/Users/yashal.imran/Downloads/weatherfiles/Murree_weather_2011_Apr.txt'
+parser(file_path)
+yearwise_temperature('2015')
+monthwise_temperature(file_path)
+charts(file_path)
